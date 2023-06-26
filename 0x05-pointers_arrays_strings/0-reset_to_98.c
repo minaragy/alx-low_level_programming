9bd44b515b406a74682575fe54b3dc94474887fc
mina <minaragy138@gmail.com>
2023-06-26 06:44:58 -0700

    d

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of a variable in another function
+ * @n: value to be changed
+ *
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
